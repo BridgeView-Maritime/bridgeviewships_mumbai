f6ccaac872cf3375238b99d85b8c34c17cba5e30
Bridgeview Maritime <141396086+BridgeView-Maritime@users.noreply.github.com>
2023-09-28 11:07:47 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose =require('mongoose');
-const mongoURL="mongodb+srv://user:user@cluster0.abfu4n6.mongodb.net/awsmumbai?retryWrites=true&w=majority"
+const mongoURL="mongodb+srv://root:root@cluster0.abfu4n6.mongodb.net/bridgemumbai?retryWrites=true&w=majority"
 const connectToMongo=()=>{
     mongoose.connect(mongoURL , ()=>{
         console.log("connected!")
